dc2673a83c2023ca68e3969436aa7a9a9349bd50
Andrew Nesbitt <andrewnez@gmail.com>
2022-05-06 11:48:18 +0100

    Add s-maxage to cache control headers

--- app/controllers/api/v1/archives_controller.rb
+++ app/controllers/api/v1/archives_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ArchivesController < Api::V1::ApplicationController
   def list
-    expires_in(60.days, public: true) # TODO this needs to be more dynamic to take into account headers from where the file was loaded
+    expires_in(60.days, public: true, "s-maxage" => 60.days) # TODO this needs to be more dynamic to take into account headers from where the file was loaded
     @archive = Archive.new(params[:url])
     render json: @archive.list_files
   end
 
   def contents
-    expires_in(60.days, public: true) # TODO this needs to be more dynamic to take into account headers from where the file was loaded
+    expires_in(60.days, public: true, "s-maxage" => 60.days) # TODO this needs to be more dynamic to take into account headers from where the file was loaded
     @archive = Archive.new(params[:url])
     render plain: @archive.file_contents(params[:path])
   end
