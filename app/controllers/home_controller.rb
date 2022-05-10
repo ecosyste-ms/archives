b520d804c5095d3aed2b2d20b26783b4bb6b8a05
Andrew Nesbitt <andrewnez@gmail.com>
2022-05-10 17:21:28 +0100

    Improved homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,30 @@
 class HomeController < ApplicationController
   def index
-    
+    @formats = [
+      {
+        name: '.tgz/.tar.gz',
+        ecosystems: ['npm', 'pub', 'cran', 'hackage', 'puppet']
+      },
+      {
+        name: '.zip',
+        ecosystems: ['go', 'elm']
+      },
+      {
+        name: '.tar',
+        ecosystems: ['hex']
+      },
+      {
+        name: '.gem',
+        ecosystems: ['rubygems']
+      },
+      {
+        name: '.nupkg',
+        ecosystems: ['nuget']
+      },
+      {
+        name: '.crate',
+        ecosystems: ['cargo']
+      },
+    ]
   end
 end
