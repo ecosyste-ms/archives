746b37ec91664ad6717c62e86594aa7d2b48aa1c
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-10 11:08:29 +0100

    Remove Faraday initializer and add test for configured user agent in HTTP requests

--- config/initializers/faraday.rb
+++
@@ -1,5 +0,0 @@
-Faraday.default_connection_options = {
-  headers: {
-    'User-Agent' => 'archives.ecosyste.ms'
-  }
-}
---
+++ config/initializers/typhoeus.rb
@@ -0,0 +1,3 @@
+Typhoeus.configure do |config|
+  config.user_agent = 'archives.ecosyste.ms'
+end
