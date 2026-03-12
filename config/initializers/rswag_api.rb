b270232e249f720d3d75e2783b00ce909d11857b
Andrew Nesbitt <andrewnez@gmail.com>
2026-03-12 11:10:13 +0000

    Fix rswag-api returning string status for Rack 3
    rswag-api 2.17.0 returns '200' (string) but Rack 3 requires an integer,
    causing ArgumentError on the openapi.yaml endpoint.

--- config/initializers/rswag_api.rb
+++ config/initializers/rswag_api.rb
@@ -1,14 +1,12 @@
 Rswag::Api.configure do |c|
-
-  # Specify a root folder where Swagger JSON files are located
-  # This is used by the Swagger middleware to serve requests for API descriptions
-  # NOTE: If you're using rswag-specs to generate Swagger, you'll need to ensure
-  # that it's configured to generate files in the same folder
   c.openapi_root = Rails.root.to_s + '/openapi'
-
-  # Inject a lamda function to alter the returned Swagger prior to serialization
-  # The function will have access to the rack env for the current request
-  # For example, you could leverage this to dynamically assign the "host" property
-  #
-  #c.openapi_filter = lambda { |swagger, env| swagger['host'] = env['HTTP_HOST'] }
 end
+
+# Patch rswag-api middleware to return integer status for Rack 3 compatibility
+# https://github.com/rswag/rswag/issues/751
+Rswag::Api::Middleware.prepend(Module.new do
+  def call(env)
+    status, headers, body = super
+    [status.to_i, headers, body]
+  end
+end)
--- test/integration/openapi_test.rb
+++ test/integration/openapi_test.rb
@@ -6,3 +6,11 @@ class OpenapiTest < ActiveSupport::TestCase
     assert_equal f.class, Hash
   end
 end
+
+class OpenapiEndpointTest < ActionDispatch::IntegrationTest
+  test 'serves openapi.yaml with integer status' do
+    get '/docs/api/v1/openapi.yaml'
+    assert_response :success
+    assert_includes response.content_type, 'yaml'
+  end
+end
